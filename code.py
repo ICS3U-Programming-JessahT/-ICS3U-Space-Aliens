13712bc783287bf05db033608fe7ef8ab0d3015d
Jessah Tumbaga <jessah.tumbaga@stu.ocsb.ca>
2023-01-12 16:28:54 +0000

    added code.py

--- code.py
+++ code.py
@@ -7,6 +7,7 @@
 import ugame
 import stage
 
+
 def game_scene():
     image_bank_background = stage.Bank.from_bmp16(space_aliens_background.bmp)
     background = stage.Grid(image_bank_background, 10, 8)
@@ -16,5 +17,7 @@ def game_scene():
     game.render_block()
     while True:
         pass
+
+
 if __name__ == "__main__":
     game_scene()
