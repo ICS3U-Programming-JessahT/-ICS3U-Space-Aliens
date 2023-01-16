3a1fa5eeb222bc2cecc60f3c4f976305c18bb45d
Jessah Tumbaga <jessah.tumbaga@stu.ocsb.ca>
2023-01-16 19:12:59 +0000

    Added code.py

--- code.py
+++ code.py
@@ -9,21 +9,21 @@ import ugame
 
 
 def game_scene():
-#  import background image
+    #  import background image
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
     image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
 
-# sheet
+    # sheet
     background = stage.Grid(image_bank_background, 10, 8)
     person = stage.Sprite(image_bank_sprites, 5, 75, 66)
 
-# display the image
+    # display the image
     game = stage.Stage(ugame.display, 60)
     game.layers = [person] + [background]
     game.render_block()
-# loop the game
+    # loop the game
     while True:
-#user input, buttons on pyBadge
+        # user input, buttons on pyBadge
         keys = ugame.buttons.get_pressed()
 
         if keys & ugame.K_X:
@@ -32,7 +32,7 @@ def game_scene():
             print("B")
         if keys & ugame.K_START:
             print("Start")
-        if keys & ugame.k_SELECT:
+        if keys & ugame.K_SELECT:
             print("Select")
         if keys & ugame.K_RIGHT:
             person.move(person.x + 1, person.y)
@@ -42,12 +42,10 @@ def game_scene():
             person.move(person.x, person.y - 1)
         if keys & ugame.K_DOWN:
             person.move(person.x, person.y + 1)
-#game logic
-#redraw sprite
+        # game logic
+        # redraw sprite
         game.render_sprites([person])
         game.tick()
-
-
 
 
 if __name__ == "__main__":
